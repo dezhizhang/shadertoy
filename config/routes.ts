65af853c52323801db5fbc44db3c8a6757121090
dezhizhang <1541609448@qq.com>
2025-02-07 19:51:46 +0800

    feat:更新shader

--- config/routes.ts
+++ config/routes.ts
@@ -5,7 +5,7 @@
  * :copyright: (c) 2025, Xiaozhi
  * :date created: 2025-01-07 23:28:56
  * :last editor: 张德志
- * :date last edited: 2025-02-06 17:42:36
+ * :date last edited: 2025-02-07 19:47:31
  */
 export default [
   {
@@ -14,44 +14,48 @@ export default [
     routes: [
       {
         path: '/',
-        redirect: '/cine-shader',
+        redirect: '/mdX3zr',
       },
       {
         path: '/cine-shader',
-        component: '@/pages/cineShader'
+        component: '@/pages/cineShader',
       },
       {
         path: '/ftt3R7',
-        component: '@/pages/ftt3R7'
+        component: '@/pages/ftt3R7',
       },
       {
-        path:'/xccbrx',
-        component:'@/pages/xccbrx'
+        path: '/xccbrx',
+        component: '@/pages/xccbrx',
       },
       {
-        path:'/wldsrn',
-        component:'@/pages/wldsrn'
+        path: '/wldsrn',
+        component: '@/pages/wldsrn',
       },
       {
-        path:'/lscczl',
-        component:'@/pages/lscczl'
+        path: '/lscczl',
+        component: '@/pages/lscczl',
       },
       {
-        path:'/xsfgrn',
-        component:'@/pages/xsfgrn'
+        path: '/xsfgrn',
+        component: '@/pages/xsfgrn',
       },
       {
-        path:'/4dl3zn',
-        component:'@/pages/4dl3zn'
+        path: '/4dl3zn',
+        component: '@/pages/4dl3zn',
       },
       {
-        path:'/tsjszh',
-        component:"@/pages/tsjszh"
+        path: '/tsjszh',
+        component: '@/pages/tsjszh',
       },
       {
-        path:'/xisbz1',
-        component:'@/pages/xisbz1'
-      }
+        path: '/xisbz1',
+        component: '@/pages/xisbz1',
+      },
+      {
+        path: '/mdX3zr',
+        component: '@/pages/mdX3zr',
+      },
     ],
   },
 ];
